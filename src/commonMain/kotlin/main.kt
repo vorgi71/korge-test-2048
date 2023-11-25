96b3db80b61ec712ca1d977c7801da6711c12f66
voa9bh <andreas.vorgeitz@me.com>
2023-11-25 21:27:18 +0100

    starting with the background

--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -1,35 +1,27 @@
-import korlibs.time.*
 import korlibs.korge.*
 import korlibs.korge.scene.*
-import korlibs.korge.tween.*
 import korlibs.korge.view.*
 import korlibs.image.color.*
-import korlibs.image.format.*
-import korlibs.io.file.std.*
 import korlibs.math.geom.*
-import korlibs.math.interpolation.*
 
-suspend fun main() = Korge(windowSize = Size(512, 512), backgroundColor = Colors["#2b2b2b"]) {
-	val sceneContainer = sceneContainer()
+suspend fun main() = Korge(
+  windowSize = Size(480, 640),
+  title = "2048",
+  backgroundColor = RGBA(254, 247, 240)
+) {
+  val sceneContainer = sceneContainer()
 
-	sceneContainer.changeTo({ MyScene() })
+  sceneContainer.changeTo { MyScene() }
 }
 
 class MyScene : Scene() {
-	override suspend fun SContainer.sceneMain() {
-		val minDegrees = (-16).degrees
-		val maxDegrees = (+16).degrees
+  override suspend fun SContainer.sceneMain() {
+    val cellSize = views.virtualWidth / 5.0
+    val fieldSize = 50 + 4 * cellSize
+    val leftIndent = (views.virtualWidth - fieldSize) / 2
+    val topIndent = 150.0
 
-		val image = image(resourcesVfs["korge.png"].readBitmap()) {
-			rotation = maxDegrees
-			anchor(.5, .5)
-			scale(0.8)
-			position(256, 256)
-		}
-
-		while (true) {
-			image.tween(image::rotation[minDegrees], time = 1.seconds, easing = Easing.EASE_IN_OUT)
-			image.tween(image::rotation[maxDegrees], time = 1.seconds, easing = Easing.EASE_IN_OUT)
-		}
-	}
+    val bgField = RoundRect(Size(fieldSize, fieldSize), RectCorners(5.0), fill = Colors["#b9aea0"])
+    addChild(bgField)
+  }
 }
