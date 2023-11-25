152e97e4ff071147fd96ca3e8818e7a38f7610b3
voa9bh <andreas.vorgeitz@me.com>
2023-11-25 20:04:19 +0100

    initial revision

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+import korlibs.korge.gradle.*
+
+plugins {
+  alias(libs.plugins.korge)
+}
+
+korge {
+  id = "com.sample.demo"
+
+// To enable all targets at once
+
+  //targetAll()
+
+// To enable targets based on properties/environment variables
+  //targetDefault()
+
+// To selectively enable targets
+
+  targetJvm()
+  targetJs()
+//  targetIos()
+
+  serializationJson()
+}
+
+
+dependencies {
+  add("commonMainApi", project(":deps"))
+  //add("commonMainApi", project(":korge-dragonbones"))
+}
+
