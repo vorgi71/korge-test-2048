0499bbcda05ea1612c304a04a3e04df9086b4410
voa9bh <andreas.vorgeitz@me.com>
2023-11-26 16:06:57 +0100

    and part 3. a tiny little buggy but playable. had to rework the animation completely.

--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -3,16 +3,20 @@ import korlibs.image.color.*
 import korlibs.image.font.*
 import korlibs.image.format.*
 import korlibs.image.text.*
+import korlibs.io.async.*
 import korlibs.io.file.std.*
 import korlibs.korge.*
+import korlibs.korge.animate.*
 import korlibs.korge.input.*
 import korlibs.korge.scene.*
 import korlibs.korge.view.*
 import korlibs.korge.view.align.*
 import korlibs.math.geom.*
+import korlibs.math.interpolation.*
 import my2048.*
 import kotlin.properties.*
 import kotlin.random.*
+import kotlin.time.Duration.Companion.seconds
 
 suspend fun main() = Korge(
   windowSize = Size(480, 640),
@@ -31,15 +35,24 @@ object MainData {
   var topIndent = 0.0
   var font: BitmapFont by Delegates.notNull()
 
+  var isAnimationRunning = false
+  var isGameOver = false
+
   val tiles = mutableMapOf<Int, Tile>()
   private var freeId = 0
-  val positionMap = PositionMap()
+  var positionMap = PositionMap()
 
   fun xOffset(x: Int) = leftIndent + 10.0 + (10 + cellSize) * x
   fun yOffset(y: Int) = topIndent + 10.0 + (10 + cellSize) * y
   fun getFreeId(): Int {
     return freeId++
   }
+
+  fun tileNumberFor(id: Int): TileNumber? {
+    return tiles[id]?.tileNumber
+  }
+
+  fun deleteTile(blockId: Int) = tiles.remove(blockId)!!.removeFromParent()
 }
 
 class MyScene : Scene() {
@@ -115,7 +128,19 @@ class MyScene : Scene() {
       }
       alignTopToBottomOf(bgBest, 5)
       alignRightToRightOf(bgField)
+
+      onOver {
+        background.color = Colors.DARKKHAKI
+      }
+      onOut {
+        background.color = RGBA(185, 174, 160)
+      }
     }
+
+    restartBlock.onClick {
+      restart()
+    }
+
     val undoBlock = container {
       val background = roundRect(Size(btnSize, btnSize), RectCorners(5.0), fill = RGBA(185, 174, 160))
       image(undoImg) {
@@ -131,29 +156,24 @@ class MyScene : Scene() {
     keys {
       down {
         when (it.key) {
-          Key.LEFT, Key.A -> moveTilesTo(Direction.LEFT)
-          Key.RIGHT, Key.D -> moveTilesTo(Direction.RIGHT)
-          Key.UP, Key.W -> moveTilesTo(Direction.TOP)
-          Key.DOWN, Key.S -> moveTilesTo(Direction.BOTTOM)
+          Key.LEFT, Key.A -> stage?.moveTilesTo(Direction.LEFT)
+          Key.RIGHT, Key.D -> stage?.moveTilesTo(Direction.RIGHT)
+          Key.UP, Key.W -> stage?.moveTilesTo(Direction.TOP)
+          Key.DOWN, Key.S -> stage?.moveTilesTo(Direction.BOTTOM)
           else -> Unit
         }
       }
     }
 
     onSwipe(20.0) {
-      when(it.direction) {
-        SwipeDirection.LEFT -> moveTilesTo(Direction.LEFT)
-        SwipeDirection.RIGHT -> moveTilesTo(Direction.RIGHT)
-        SwipeDirection.TOP -> moveTilesTo(Direction.TOP)
-        SwipeDirection.BOTTOM -> moveTilesTo(Direction.BOTTOM)
+      when (it.direction) {
+        SwipeDirection.LEFT -> stage?.moveTilesTo(Direction.LEFT)
+        SwipeDirection.RIGHT -> stage?.moveTilesTo(Direction.RIGHT)
+        SwipeDirection.TOP -> stage?.moveTilesTo(Direction.TOP)
+        SwipeDirection.BOTTOM -> stage?.moveTilesTo(Direction.BOTTOM)
       }
     }
 
-  }
-
-  fun moveTilesTo(direction: Direction) {
-    println("moveTilesTo(${direction.name})")
-    // @todo: next step
   }
 }
 
@@ -173,5 +193,116 @@ fun Container.generateBlock() {
   val freePosition = MainData.positionMap.getRandomFreePosition() ?: return
   val number = if (Random.nextDouble() < 0.9) TileNumber.ZERO else TileNumber.ONE
   val newId = createTile(number, freePosition)
-  MainData.positionMap.setId(freePosition.x, freePosition.y, newId)
+  MainData.positionMap[freePosition.x, freePosition.y] = newId
+}
+
+fun Stage.moveTilesTo(direction: Direction) {
+  if (MainData.isAnimationRunning) {
+    return
+  }
+  if (!MainData.positionMap.hasAvailableMoves()) {
+    if (MainData.isGameOver) {
+      return
+    }
+    MainData.isGameOver = true
+    showGameOver {
+      MainData.isGameOver = false
+      restart()
+    }
+  }
+
+  val moves = mutableListOf<Pair<Int, Position>>()
+  val merges = mutableListOf<Triple<Int, Int, Position>>()
+
+  val newMap = MainData.positionMap.calculateNewMap(MainData.positionMap.copy(), direction, moves, merges)
+
+  if (MainData.positionMap != newMap) {
+    MainData.isAnimationRunning = true
+    showAnimation(moves, merges) {
+      // when animation ends
+      MainData.positionMap = newMap
+      generateBlock()
+      MainData.isAnimationRunning = false
+    }
+    MainData.isAnimationRunning = false
+  }
+}
+
+fun Stage.showAnimation(
+  moves: List<Pair<Int, Position>>,
+  merges: List<Triple<Int, Int, Position>>,
+  onEnd: () -> Unit
+) {
+  launchImmediately {
+    animate {
+      parallel {
+        moves.forEach { (id, pos) ->
+          val tile:Tile= MainData.tiles[id]!!
+          moveTo(tile,MainData.xOffset(pos.x),MainData.yOffset(pos.y), 0.15.seconds, Easing.LINEAR)
+        }
+        merges.forEach { (id1, id2, pos) ->
+          sequence {
+            parallel {
+              moveTo(MainData.tiles[id1]!!,MainData.xOffset(pos.x),MainData.yOffset(pos.y), 0.15.seconds, Easing.LINEAR)
+              moveTo(MainData.tiles[id2]!!,MainData.xOffset(pos.x),MainData.yOffset(pos.y), 0.15.seconds, Easing.LINEAR)
+            }
+            block {
+              val nextNumber = MainData.tileNumberFor(id1)!!.next()
+              MainData.deleteTile(id1)
+              MainData.deleteTile(id2)
+              createTileWithId(id1, nextNumber, pos)
+            }
+            sequenceLazy {
+              scaleTo(MainData.tiles[id1]!!,1.0,1.0)
+            }
+          }
+        }
+      }
+      block {
+        onEnd()
+      }
+    }
+  }
+}
+
+
+fun Container.showGameOver(onRestart: () -> Unit) = container {
+  fun restart() {
+    this@container.removeFromParent()
+    onRestart()
+  }
+
+  position(MainData.leftIndent, MainData.topIndent)
+
+  roundRect(Size(MainData.fieldSize, MainData.fieldSize), RectCorners(5.0), fill = Colors["#FFFFFF33"])
+  text("Game Over", 60.0, Colors.BLACK, MainData.font) {
+    centerBetween(0.0, 0.0, MainData.fieldSize, MainData.fieldSize)
+    y -= 60
+  }
+  text("Try again", 40.0, font = MainData.font) {
+    centerBetween(0.0, 0.0, MainData.fieldSize, MainData.fieldSize)
+    y += 20
+    textSize = 40.0
+    color = RGBA(0, 0, 0)
+    onOver { color = RGBA(90, 90, 90) }
+    onOut { color = RGBA(0, 0, 0) }
+    onDown { color = RGBA(120, 120, 120) }
+    onUp { color = RGBA(120, 120, 120) }
+    onClick { restart() }
+  }
+
+  keys.down {
+    when (it.key) {
+      Key.ENTER, Key.SPACE -> restart()
+      else -> Unit
+    }
+  }
+}
+
+fun Container.restart() {
+  println("in restart()")
+  MainData.positionMap = PositionMap()
+  MainData.tiles.values.forEach { it.removeFromParent() }
+  MainData.tiles.clear()
+  generateBlock()
 }
--- src/commonMain/kotlin/my2048/PositionMap.kt
+++ src/commonMain/kotlin/my2048/PositionMap.kt
@@ -7,6 +7,10 @@ import kotlin.random.*
 data class Position(val x: Int, val y: Int)
 
 class PositionMap(private val array: IntArray2 = IntArray2(4, 4, -1)) {
+  constructor(positionMap: PositionMap) : this() {
+    positionMap.array.each { x, y, v -> this.array[x, y] = v }
+  }
+
   fun getOrNull(x: Int, y: Int): Position? {
     if (array.get(x, y) != -1) {
       return Position(x, y)
@@ -18,9 +22,9 @@ class PositionMap(private val array: IntArray2 = IntArray2(4, 4, -1)) {
     MainData.tiles[id]?.tileNumber?.ordinal ?: -1
   } ?: -1
 
-  fun getId(x: Int, y: Int): Int = array.get(x, y)
+  operator fun get(x: Int, y: Int): Int = array.get(x, y)
 
-  fun setId(x: Int, y: Int, id: Int) {
+  operator fun set(x: Int, y: Int, id: Int) {
     array.set(x, y, id)
   }
 
@@ -35,26 +39,92 @@ class PositionMap(private val array: IntArray2 = IntArray2(4, 4, -1)) {
     return this.array.data.hashCode()
   }
 
-  fun freePositions(): List<Position> {
-    val freePositions: MutableList<Position> = mutableListOf()
-    array.each { x, y, id ->
-      if (id == -1) {
-        freePositions.add(Position(x, y))
+  fun getRandomFreePosition(): Position? {
+    val quantity = array.count { it == -1 }
+    if (quantity == 0) return null
+    val chosen = Random.nextInt(quantity)
+    var current = -1
+    array.each { x, y, value ->
+      if (value == -1) {
+        current++
+        if (current == chosen) {
+          return Position(x, y)
+        }
       }
     }
-    return freePositions
+    return null
   }
 
-  fun isFull(): Boolean {
-    return array.count { id -> id == -1 } == 0
-  }
-
-  fun getRandomFreePosition(): Position? {
-    val freePositions = freePositions()
-    if (freePositions.isEmpty()) {
-      return null
+  fun hasAvailableMoves(): Boolean {
+    array.each { x, y, _ ->
+      if (hasAdjacentEqualPosition(x, y)) return true
     }
-    val chosenPositionIndex = Random.nextInt(freePositions.size)
-    return freePositions[chosenPositionIndex]
+    return false
+  }
+
+  private fun hasAdjacentEqualPosition(x: Int, y: Int) = getNumber(x, y).let {
+    it == getNumber(x - 1, y) || it == getNumber(x + 1, y) || it == getNumber(x, y - 1) || it == getNumber(x, y + 1)
+  }
+
+  fun copy(): PositionMap {
+    return PositionMap(this)
+  }
+
+  fun calculateNewMap(
+    map: PositionMap,
+    direction: Direction,
+    moves: MutableList<Pair<Int, Position>>,
+    merges: MutableList<Triple<Int, Int, Position>>
+  ): PositionMap {
+    val newMap = PositionMap()
+    val startIndex = when (direction) {
+      Direction.LEFT, Direction.TOP -> 0
+      Direction.RIGHT, Direction.BOTTOM -> 3
+    }
+    var columnRow = startIndex
+
+    fun newPosition(line: Int) = when (direction) {
+      Direction.LEFT -> Position(columnRow++, line)
+      Direction.RIGHT -> Position(columnRow--, line)
+      Direction.TOP -> Position(line, columnRow++)
+      Direction.BOTTOM -> Position(line, columnRow--)
+    }
+
+    for (line in 0..3) {
+      var curPos = map.getNotEmptyPositionFrom(direction, line)
+      columnRow = startIndex
+      while (curPos != null) {
+        val newPos = newPosition(line)
+        val curId = map[curPos.x, curPos.y]
+        map[curPos.x, curPos.y] = -1
+
+        val nextPos = map.getNotEmptyPositionFrom(direction, line)
+        val nextId = nextPos?.let { map[it.x, it.y] }
+        //two blocks are equal
+        if (nextId != null && MainData.tileNumberFor(curId) == MainData.tileNumberFor(nextId)) {
+          //merge these blocks
+          map[nextPos.x, nextPos.y] = -1
+          newMap[newPos.x, newPos.y] = curId
+          merges += Triple(curId, nextId, newPos)
+        } else {
+          //add old block
+          newMap[newPos.x, newPos.y] = curId
+          moves += Pair(curId, newPos)
+        }
+        curPos = map.getNotEmptyPositionFrom(direction, line)
+      }
+    }
+    return newMap
+  }
+
+  fun getNotEmptyPositionFrom(direction: Direction, line: Int): Position? {
+    when (direction) {
+      Direction.LEFT -> for (i in 0..3) getOrNull(i, line)?.let { return it }
+      Direction.RIGHT -> for (i in 3 downTo 0) getOrNull(i, line)?.let { return it }
+      Direction.TOP -> for (i in 0..3) getOrNull(line, i)?.let { return it }
+      Direction.BOTTOM -> for (i in 3 downTo 0) getOrNull(line, i)?.let { return it }
+    }
+    return null
   }
 }
+
